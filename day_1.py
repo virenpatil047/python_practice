3d6e1d5257f40034a496f3ff880698d6e4abe317
Wren <virenpatil047@gmail.com>
2026-04-14 23:08:33 +0530

    Day - 1
    1st commit

---
+++ day_1.py
@@ -0,0 +1,10 @@
+#print("Hello, World!")
+
+# print("Hello" + input("Whats ur name ??") + "!")
+
+# name=input("UR NAME ??\n")
+# print(name + " len : "+ str(len(name)))
+
+username = input("Enter your name : ")
+length = len(username)
+print(length)
