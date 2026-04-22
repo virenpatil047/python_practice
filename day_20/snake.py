c19b9b2d13328f781ccb2b8ff30aadba5e2d1382
Wren <virenpatil047@gmail.com>
2026-04-22 21:27:38 +0530

    Snake Game Part - 1
    Implementing snake game using Turtle graphics.

---
+++ day_20/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+LEFT = 180
+RIGHT = 0
+UP = 90
+DOWN = 270
+
+class Snake:
+    
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+        
+    def create_snake(self):
+        x_index = 0
+        for i in range(0, 3):
+            segment = Turtle()
+            segment.penup()
+            segment.shape("square")
+            segment.color("white")
+            segment.goto(x_index, 0)
+            self.snake.append(segment)
+            x_index -= 20
+            
+    def move(self):
+        for i in range(len(self.snake)-1, 0, -1):
+            self.snake[i].goto(self.snake[i-1].xcor(), self.snake[i-1].ycor())
+        self.head.forward(20)
+        
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    
+    def right(self):
+        if self.head.heading() != LEFT:
+           self.head.setheading(RIGHT)
---
+++ day_20/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+#     for i in range(len(snake)-1, 0, -1):
+#         snake[i].goto(snake[i-1].xcor(), snake[i-1].ycor())
+#     snake[0].forward(20)
