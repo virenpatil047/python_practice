4ec6560760ae1238a5bd7e15f7f350ae1ebf7bf5
Wren <virenpatil047@gmail.com>
2026-05-20 19:22:52 +0530

    List & Dictionary comprehension

--- day_26/main.py
+++ day_26/main.py
@@ -6,6 +6,13 @@
 # new_lst = [n * 2 for n in range(1, 5)]
 # print(new_lst)
 
+# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+# new_names = [name.upper() for name in names if len(name) > 5]
+# print(new_names)
+
+import random
 names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-new_names = [name.upper() for name in names if len(name) > 5]
-print(new_names)
+student_scores = {name : random.randint(1, 100) for name in names}
+passed_students = {name : marks for (name, marks) in student_scores.items() if marks > 60}
+print(student_scores)
+print(passed_students)
