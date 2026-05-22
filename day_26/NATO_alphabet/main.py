e49f3b61eee3c2363084c11cb2359b1c5e455beb
Wren <virenpatil047@gmail.com>
2026-05-23 04:04:49 +0530

    NATO alphabet
    Updated exception handling and list comprehensions.

--- day_26/NATO_alphabet/main.py
+++ day_26/NATO_alphabet/main.py
@@ -1,13 +1,21 @@
 import pandas as pd
 from pathlib import Path
 
-#dictionary in NATO format:
+# Dictionary in NATO format.
 BASE_PATH = Path(__file__).parent
 df = pd.read_csv(BASE_PATH / "nato_phonetic_alphabet.csv")
 nato_alpha = {row.letter : row.code for (index, row) in df.iterrows()}
 # {"A": "Alfa", "B": "Bravo"}
 
-#list of the phonetic code words from a word that the user inputs.
-name = input("Enter name : ")
-nato_name = {word.upper() : nato_alpha[word.upper()] for word in name}
-print(nato_name)
+# List of the phonetic code words from a word that the user inputs.
+def nato():
+    name = input("Enter name : ").upper()
+    try:
+        nato_name = {word : nato_alpha[word] for word in name}
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato()
+    else:
+        print(nato_name)
+
+nato()
