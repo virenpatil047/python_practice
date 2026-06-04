fa1f0761661555cbe456731e6f6fecce553356ed
Wren <virenpatil047@gmail.com>
2026-06-04 11:31:58 +0530

    Kanye Quote
    Gets a response from an API, which returns a random Kanye West quotes.

---
+++ day_33/kanye_quotes/main.py
@@ -0,0 +1,34 @@
+import requests
+from tkinter import *
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent
+
+def set_quote(quote):
+    canvas.itemconfig(quote_text, text=quote)
+
+def get_quote():
+    response = requests.get(url="https://api.kanye.rest")
+    quote = response.json()["quote"]
+    set_quote(quote)
+
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file=BASE_PATH / "background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file=BASE_PATH / "kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+get_quote()
+
+
+window.mainloop()
